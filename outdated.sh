405f4371cf82212692abb4ac89fa8cb5f2d2c489
Brian May <brian@linuxpenguins.xyz>
2021-06-25 10:47:27 +1000

    Add script to check for outdated charts

---
+++ outdated.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+for chart in charts/*; do
+    test -f "$chart/Chart.yaml" || continue
+
+    helm dependency update "$chart" > /dev/null
+
+    while IFS= read -r dependency; do
+        test -n "$dependency" || continue
+
+        subchart="$(echo "$dependency" | awk '{print $1}')"
+        current="$(echo "$dependency" | awk '{print $2}')"
+        repo="$(echo "$dependency" | awk '{print $3}')"
+        latest=$(helm inspect chart --repo "$repo" "$subchart" | yq e '.version' -)
+
+        if [ "$current" == "$latest" ]; then
+            true
+        else
+            printf "%s: Consider upgrading %s: %s -> %s.\\n" \
+                "$chart" "$subchart" "$current" "$latest"
+        fi
+    done < <(helm dep list "$chart" | tail -n +2)
+done
